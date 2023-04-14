ba094d1a3b543979bcb634cd8d0df5772d1f367c
Karan <karanpawar0332@gmail.com>
2023-04-14 21:06:02 +0530

    update test file

--- test/appTest.test.js
+++ test/appTest.test.js
@@ -2,9 +2,6 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index.js';
 
-// import { expect } from 'chai';
-// import { describe, it } from 'mocha';
-// import request from 'supertest';
 
 
 chai.use(chaiHttp);
@@ -27,22 +24,6 @@ chai.use(chaiHttp);
     });
 
   
-  /**
-         * Test Get /api/posts/{id}
-         * RETURN: a post with number of likes and comment
-         */
-  describe('Get Post by id',  () => {
-    it('should return  a post with number of likes and comments ',  () => {
-        const postid = "";
-        chai.request(app)
-            .post('/api/posts'+ postid)
-            .send({"username": "karan", "password": "karan"})
-            .then((res) => {
-              // console.log(res.body);
-              res.should.have.status(200);
-              res.body.should.have.property('JWT_token')
-              done();
-            });
-    });
+  
 
   });
